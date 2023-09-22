e1c2b1737282f732eddf357b982695738599707f
杨川 <471448446@qq.com>
2023-09-22 19:23:02 +0800

    taro demo

---
+++ taro/hello-taro/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup() {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  },
+  mounted() {
+    console.log("Hello taro")
+    // 测试直接调用weix的api
+    wx.request({
+      url: "https://www.baidu.com",
+      method: 'GET',
+      success: (res) => {
+        console.log(res)
+      },
+      fail: (err) => {
+        console.log("fail:" + err.errMsg + "," + err.errno)
+      }
+    })
+  }
+}
+</script>
