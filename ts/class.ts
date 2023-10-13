4279638c36875a6098f2d7229a5b405e57e9ac90
杨川 <471448446@qq.com>
2023-10-13 19:24:59 +0800

    数据解析

--- ts/class.ts
+++ ts/class.ts
@@ -9,3 +9,21 @@ class Point {
 }
 let point1 = new Point(1, 2);
 console.log(point1);
+console.log(typeof point1);
+console.log(point1.x);
+
+// 尝试解析json
+let json = '{"x":1,"y":2}'
+const parse1:Point = Object.assign(new Point(0,0), JSON.parse(json))
+console.log(typeof parse1);
+console.log("parse1:" + parse1);
+console.log(parse1.x);
+console.log(JSON.stringify(parse1));
+// 
+class XX{
+    x:number = 0;
+    y:number = 0;
+}
+const parse2:XX = Object.assign(new XX(), JSON.parse(json))
+console.log(parse2);
+console.log("stringify:"+JSON.stringify(parse2));
